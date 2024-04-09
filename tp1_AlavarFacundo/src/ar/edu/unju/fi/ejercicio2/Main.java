1cc25ea54ac2ae48cdf3dba333fc1fdbe2cbab88
f4cu22 <fascundo96@gmail.com>
2024-04-09 17:32:11 -0300

    Se arreglo el ejercicio 1 y se agrega el ejercicio 2

---
+++ tp1_AlavarFacundo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais;
+		byte edad;
+		int altura;
+		float precio;
+		String telefono;
+		float coseno;
+		
+		pais="Alemania";
+		System.out.println("El nombre de un país: "+pais+".");
+		edad=22;
+		System.out.println("La edad de una persona: "+edad+" años.");
+		altura=330;
+		System.out.println("La altura de un edificio: "+altura+"m.");
+		precio=(float)559.99;
+		System.out.println("El precio de un producto de supermercado: "+precio+"$.");
+		telefono="+54 388 4123456";
+		System.out.println("Número de teléfono.: "+telefono+".");
+		coseno=(float)0.877582562;
+		System.out.println("El cálculo de coseno de 0.5: "+coseno+".");
+		
+	}
+
+}
