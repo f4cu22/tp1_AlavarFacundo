8428c9a0cf24fcaf8cb3c4aab233e68efe78d820
f4cu22 <fascundo96@gmail.com>
2024-04-09 13:26:12 -0300

    Se resolvio el ejercicio 1

--- tp1_AlavarFacundo/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_AlavarFacundo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,9 +1,22 @@
 package ar.edu.unju.fi.ejercicio1;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo.");
+		//Ejercicio 1 - Calcular el promedio y guarde el resultado en otra variable.
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese 5 numeros enteros a promediar: ");
+		Integer num1 =scanner.nextInt();
+		Integer num2 =scanner.nextInt();
+		Integer num3 =scanner.nextInt();
+		Integer num4 =scanner.nextInt();
+		Integer num5 =scanner.nextInt();
+		float promedio=(num1+num2+num3+num4+num5)/5;
+		System.out.println("Los numeros a promediar son: "+num1+", "+num2+", "+num3+", "+num4+", "+num5+".");
+		System.out.println("El promedio es: "+promedio);
+		scanner.close();
 
 	}
 
