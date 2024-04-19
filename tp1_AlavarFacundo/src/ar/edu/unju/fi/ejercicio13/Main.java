035986a1dc18aa62e3ca0ff72b424d1e9d2c0af4
f4cu22 <fascundo96@gmail.com>
2024-04-19 12:47:24 -0300

    Se agrega ejercicio 13 resuelto

---
+++ tp1_AlavarFacundo/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.ejercicio13;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		//array de 8 enteros
+        int[] miArray = new int[8];
+        
+        // Pedir al usuario que ingrese 8 números enteros
+        for (int i = 0; i < miArray.length; i++) {
+            System.out.print("Ingresa un número entero para la posición " + i + ": ");
+            miArray[i] = scanner.nextInt();
+        }
+
+        // índice y el valor 
+        System.out.println("Índice\tValor");
+        for (int i = 0; i < miArray.length; i++) {
+            System.out.println(i + "\t" + miArray[i]);
+        }
+	}
+
+}
